344dba621efa45626e6c32af946e77a7f4f20d46
tentendotz <tenten102matsuyama@gmail.com>
2023-02-17 23:30:42 +0900

    Implement tableView data source methods

--- Todoey-CoreData/Controllers/TodoListViewController.swift
+++ Todoey-CoreData/Controllers/TodoListViewController.swift
@@ -7,9 +7,18 @@
 
 import UIKit
 
+// Dummy class
+class Item {
+    let title = ""
+    var done = false
+}
+
+
 class TodoListViewController: UITableViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
+    
+    var itemArray = [Item]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,3 +44,24 @@ class TodoListViewController: UITableViewController {
     
 }
 
+
+//MARK: - TableView DataSource & Delegate Methods
+
+extension TodoListViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = item.title
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+}
