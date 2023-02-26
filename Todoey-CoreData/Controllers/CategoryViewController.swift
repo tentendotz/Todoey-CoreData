f304a7290a0655262c88edbb0e1d7bdf7eca3a79
tentendotz <tenten102matsuyama@gmail.com>
2023-02-26 19:05:30 +0900

    Implement tableView data source methods

--- Todoey-CoreData/Controllers/CategoryViewController.swift
+++ Todoey-CoreData/Controllers/CategoryViewController.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
+class Category {
+    var name = ""
+}
+
 class CategoryViewController: UITableViewController {
+    
+    var categories = [`Category`]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,21 +34,26 @@ class CategoryViewController: UITableViewController {
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
     }
     
-    // MARK: - TableView DataSource & Delegate Methods
+}
 
+
+extension CategoryViewController {
+    
+    // MARK: - TableView DataSource & Delegate Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return categories.count
     }
     
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = categories[indexPath.row].name
+        cell.contentConfiguration = content
+        
         return cell
     }
-    */
+    
 
     /*
     // MARK: - Navigation Transitions
